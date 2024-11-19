fd75da01415fd34e5ea943e1bba6bacf20109604
Narets <49039754+Naretss@users.noreply.github.com>
2024-11-19 10:09:08 +0700

    Update App.js utf8

--- src/App.js
+++ src/App.js
@@ -7,8 +7,18 @@ function App() {
 
   const handleConvert = () => {
     try {
-      const decodedString = atob(base64);
-      setString(decodedString);
+      const decodedString = new TextDecoder('utf-8').decode(Uint8Array.from(atob(base64), c => c.charCodeAt(0)));
+      let formattedString = decodedString;
+
+      // Check if the decoded string is JSON
+      try {
+        const jsonObject = JSON.parse(decodedString);
+        formattedString = JSON.stringify(jsonObject, null, 2); // Format JSON with 2 spaces indentation
+      } catch (e) {
+        // Not a JSON string, do nothing
+      }
+
+      setString(formattedString);
     } catch (error) {
       alert('Invalid Base64 string');
     }
@@ -18,10 +28,9 @@ function App() {
     <div className="App">
       <h1>Base64 Decode</h1>
       <p>Decode Base64 string or use the Base64 to File tool for large files</p>
-      <div className="input-container">
-        <label htmlFor="base64-input">Input</label>
-        <textarea
-          id="base64-input"
+      <div>
+        <input
+          type="text"
           value={base64}
           onChange={(e) => setBase64(e.target.value)}
           placeholder="Enter Base64"
@@ -35,13 +44,10 @@ function App() {
         </div>
       )}
       <footer className="footer">
-
         <p>Narets Ng</p>
-
         <a href="https://www.facebook.com/profile.php?id=100001005871414">
           <img src={logo} className='logo' alt="Logo" />
         </a>
-
       </footer>
     </div>
   );
