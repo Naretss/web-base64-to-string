fc156c3acd8ea249441e5aa197b81dd7877777e0
pisichi <win.pisichi@gmail.com>
2025-09-29 14:16:36 +0700

    format orgmsg fix

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,28 +1,17 @@
 import { base64ToUtf8 } from "./decode";
 import beautify from "js-beautify";
 
-const formatXML = (xmlString, decodeOriginalMessage) => {
-  // Basic XML validation
-  try {
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(xmlString, "text/xml");
-    if (xmlDoc.getElementsByTagName("parsererror").length > 0) {
-      throw new Error("Invalid XML syntax");
-    }
-  } catch (e) {
-    throw new Error("Invalid XML syntax: " + e.message);
-  }
-
-  let xml = xmlString;
+export const formatXML = (xmlString, decodeOriginalMessage) => {
+  const PADDING = "  ";
+  const reg = /(>)(<)(\/*)/g;
+  let xml = xmlString.replace(reg, "$1\n$2$3");
 
   if (decodeOriginalMessage) {
     xml = xml.replace(/<([^>]*OrgMsg[^>]*)>([^<]+)<\/\1>/g, (match, tagName, base64Content) => {
       try {
         const decoded = base64ToUtf8(base64Content);
         const formattedDecoded = formatXML(decoded, decodeOriginalMessage); // recursive formatting
-        return `<${tagName}>
-${formattedDecoded}
-</${tagName}>`;
+        return `<${tagName}>\n${formattedDecoded}\n</${tagName}>`;
       } catch (e) {
         console.warn("Failed to decode base64 content:", e);
         return match;
@@ -30,7 +19,26 @@ ${formattedDecoded}
     });
   }
 
-  return beautify.html(xml, { indent_size: 2 });
+  let formatted = "";
+  let pad = 0;
+
+  xml.split("\n").forEach((node) => {
+    let indent = 0;
+    if (node.match(/.+<\/\w[^>]*>$/)) {
+      indent = 0;
+    } else if (node.match(/^<\/\w/)) {
+      if (pad !== 0) pad -= 1;
+    } else if (node.match(/^<\w[^>]*[^/]>.*$/)) {
+      indent = 1;
+    } else {
+      indent = 0;
+    }
+
+    formatted += PADDING.repeat(pad) + node + "\n";
+    pad += indent;
+  });
+
+  return beautify.html(formatted, { indent_size: 2 });
 };
 
 const formatJSON = (jsonString) => {
